deb7ea6ea66ccc67abe07017710e6be40badded8
reifocs <alainvincentesc@gmail.com>
2020-06-08 15:32:33 +0200

    Updates

---
+++ precache-manifest.7c5ff0fa8b0d590aefea0cab04573de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc508ed4e2d1712720e155b9a7f5cf63",
+    "url": "/react-quizz-app/index.html"
+  },
+  {
+    "revision": "b775de30ba8af3455d77",
+    "url": "/react-quizz-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89bb6e97a9d60d84121e",
+    "url": "/react-quizz-app/static/js/2.186208b9.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/react-quizz-app/static/js/2.186208b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b775de30ba8af3455d77",
+    "url": "/react-quizz-app/static/js/main.e73b1058.chunk.js"
+  },
+  {
+    "revision": "108daafa74ccbca3d794",
+    "url": "/react-quizz-app/static/js/runtime-main.da41d782.js"
+  }
+]);
